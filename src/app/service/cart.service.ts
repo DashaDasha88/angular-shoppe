96adaf99f7da4da3177fb89db4ce8668780faba1
Dasha <dasha.kotova.mail@gmail.com>
2021-12-21 20:22:46 -0500

    add cart service

---
+++ src/app/service/cart.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  public cartItemList : any = []
+  public productList = new BehaviorSubject<any>([]);
+
+  constructor() { }
+
+  getProducts(){
+    return this.productList.asObservable();
+  }
+
+  setProduct(product: any) {
+    this.cartItemList.push(...product)
+  }
+}
